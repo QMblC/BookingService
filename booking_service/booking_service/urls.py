3ffa263bb7b4dc6527ef4f9d65ebd6826e1e1ae2
QMblC <kurtovnevg@gmail.com>
2024-05-21 10:48:30 +0500

    TryingDjango

--- booking_service/booking_service/urls.py
+++ booking_service/booking_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_test project.
+URL configuration for booking_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import view
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-	path('', view.home, name='django_test-home'),
-	path('about/', view.about, name='about-club'),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls'))
 ]
